d57217b2e925679365247507f4fca8c3790a2086
Ralph Largo <devlargs@gmail.com>
2020-01-26 03:02:47 +0800

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import App from "./App.vue";
+import Categories from "./pages/Categories.vue";
+import Home from "./pages/Home.vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/categories",
+    component: Categories
+  },
+  {
+    path: "/",
+    component: Home
+  }
+];
+
+const router = new VueRouter({
+  routes,
+  mode: "history"
+});
 
 new Vue({
-	el: '#app',
-	render: h => h(App)
+  el: "#app",
+  router,
+  render: h => h(App)
 });
---
+++ src/pages/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <a href="/categories">Categories</a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../scss/_variables.scss";
+</style>
